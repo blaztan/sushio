9f71fbd739012982b7a1ab66dd2c32bacc594668
blaztan <periksoon191@gmail.com>
2020-12-30 21:09:42 +0100

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  siteMetadata: {
+    title: `Sushio`,
+    description: `Japanese Restaurant project`,
+    author: `@gatsbyjs`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-postcss",
+      options: {
+        postCssPlugins: [require("tailwindcss")("./tailwind.config.js")],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: { tailwind: true },
+    },
+  ],
+}
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false,
+  theme: {
+    extend: {
+      colors: {
+        primary: "#362E74",
+        secondary: "#49742E",
+        main: "#FFF5EC",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
